9d75e5f2748a3758be3df830cd573ae225125ec8
luigi <luigi.toziani@gmail.com>
2025-01-08 18:21:40 +0100

    Remove useless code

--- alb.tf
+++ alb.tf
@@ -150,31 +150,6 @@ resource "aws_lb_listener" "http" {
   }
 }
 
-# ECS Service
-resource "aws_ecs_service" "wordpress" {
-  name            = "wordpress"
-  cluster         = aws_ecs_cluster.wordpress.id
-  task_definition = aws_ecs_task_definition.wordpress.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    subnets          = aws_subnet.public[*].id
-    security_groups  = [aws_security_group.ecs.id]
-    assign_public_ip = true
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.wordpress.arn
-    container_name   = "wordpress"
-    container_port   = 80
-  }
-
-  tags = {
-    scope = "claranet"
-  }
-}
-
 # Add an output for the ALB DNS name
 output "wordpress_url" {
   description = "WordPress URL"
