b65532eea17b1eb5a06fbe40a6d371f98a0671c8
luigi <luigi.toziani@gmail.com>
2025-01-09 17:19:02 +0100

    Enable iam_database_auth

--- database.tf
+++ database.tf
@@ -8,7 +8,7 @@ resource "aws_db_instance" "main" {
   username            = var.db_username
   password            = var.db_password
   skip_final_snapshot = true
-
+  iam_database_authentication_enabled = true
   db_subnet_group_name   = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.rds.id]
 
