29c179cde3a4887281277c978b33d96d507aa209
luigi <luigi.toziani@gmail.com>
2025-01-11 23:31:03 +0100

    Configure cpu and memory

--- ecs.tf
+++ ecs.tf
@@ -97,8 +97,8 @@ resource "aws_ecs_task_definition" "wordpress" {
   family                   = "wordpress"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = 256
-  memory                   = 512
+  cpu                      = 512
+  memory                   = 1024
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   task_role_arn            = aws_iam_role.ecs_task_role.arn
 
@@ -295,6 +295,6 @@ resource "aws_appautoscaling_policy" "wordpress_cpu" {
     }
     target_value       = 50.0
     scale_in_cooldown  = 60
-    scale_out_cooldown = 60
+    scale_out_cooldown = 15
   }
 }
