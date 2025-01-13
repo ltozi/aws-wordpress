6f623aac2e988f2de9ba4382db6694d5c004b81d
luigi <luigi.toziani@gmail.com>
2025-01-13 09:36:16 +0100

    Upgrade aws provider

--- provider.tf
+++ provider.tf
@@ -3,13 +3,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
   }
 
   # This bucket must already exist. See pre requirements in README.md for the command to create it.
   backend "s3" {
     bucket = "terraform-wordpress-state"
+    key = "wordpress"
   }
 
 }
